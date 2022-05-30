735ed319f645a794ed76a511cb980eb05c8f7acc
Tomás Oropeza <taoh75@gmail.com>
2022-05-30 12:33:47 -0500

    Navbar
    Se separa el navbar del diseño de la plantilla del sistema para administrarla aparte.

---
+++ templates/layouts/navbar.php
@@ -0,0 +1,23 @@
+<!-- SE INCLUYE LA BARRA DE NAVEGACION PRINCIPAL -->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">CATPROD 1.0</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExample03">
+            <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Categorías</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Productos</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- CONTENIDO DE LA PÁGINA PRINCIPAL -->
